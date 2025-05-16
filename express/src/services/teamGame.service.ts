70c42c492213b158abb00afa9d5f4bdc83e85f42
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-05-16 16:56:21 +0200

    start a new route to create a game

--- express/src/services/teamGame.service.ts
+++ express/src/services/teamGame.service.ts
@@ -1,3 +1,17 @@
-export class TeamGameService {
+import { TeamGame } from "../entities/TeamGame.entity";
+import { AppDataSource } from "../config/database";
+import { Repository } from "typeorm";
 
+
+export class TeamGameService {
+    private teamGameRepository: Repository<TeamGame>;
+
+        constructor() {
+            this.teamGameRepository = AppDataSource.getRepository(TeamGame);
+        }
+
+    async createANewGame(gameData: string): Promise<TeamGame> {
+        return await this.teamGameRepository.create({
+        })
+    }
 }
