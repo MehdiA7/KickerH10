2a1ab9ac347999a548b2aa3b08f67bd02aafc810
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-05-17 14:08:02 +0200

    add verification

--- express/src/controllers/game.controller.ts
+++ express/src/controllers/game.controller.ts
@@ -8,6 +8,23 @@ export class GameController {
     static async register(req: Request, res: Response) {
         try {
             const theBody: GameData = req.body;
+
+            if (
+                !theBody.looser ||
+                !theBody.player1 ||
+                !theBody.player2 ||
+                !theBody.score1 ||
+                !theBody.score2 ||
+                !theBody.winner === undefined
+            ) {
+                res.status(400).send({
+                    success: false,
+                    message: "All field is required"
+                });
+                return;
+            }
+            
+
         } catch (error) {
             
         }
