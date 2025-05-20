d0192d43129d9f0a455309b16c2a95740b30253a
Alex <alexvdbr6@gmail.com>
2025-05-20 16:35:56 +0200

    friend view advencement

--- my-app/app/user/[username]/page.tsx
+++ my-app/app/user/[username]/page.tsx
@@ -2,36 +2,42 @@
 import ProfileOverview from "@/components/molecules/ProfileOverview";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import React from "react";
-import { FetchRecentUserMatch, FetchUserProfile, FetchAddNewFriend } from "../serverAction/fetchUsers";
+import React, { useEffect, useState } from "react";
+import { FetchRecentUserMatch, FetchUserProfile, FetchAddNewFriend } from "../../serverAction/fetchUsers";
 import RecentGame from "@/components/organisms/RecentGame";
 import SearchBar from "@/components/molecules/SearchBar";
-
+import { useParams } from "next/navigation";
+import { useRouter } from 'next/router';
 
 
 const UserPage = () => {
-    
-    let idFriend = 0 
-    let idUser = 1 // get user id in jwt
+    const params = useParams();
+    const username  = params.username;
+
+    console.log(username)
+
+    let idFriend = 0;
+    let idUser = 1; // get user id in jwt
 
     return (
         <>  
         
             <div className="flex flex-col items-center mb-2">
-                <SearchBar/>
+                <SearchBar />
             </div>
             <div className="flex flex-col items-center">
+                <div>{username}</div>
                 <ProfileOverview FetchUserProfile={FetchUserProfile(idFriend)} />
             </div>
             <article>
-                <RecentGame FetchRecentUserMatch={FetchRecentUserMatch(idFriend)}/>
+                <RecentGame FetchRecentUserMatch={FetchRecentUserMatch(idFriend)} />
             </article>
             <div className="flex w-full justify-center">
-                <Button className="h-15 mb-5" size="lg"  onClick={() => FetchAddNewFriend(idUser, idFriend)}>
+                <Button className="h-15 mb-5" size="lg" onClick={() => FetchAddNewFriend(idUser, idFriend)}>
                     Add friend
                 </Button>
             </div>
-            <div className="flex justify-center space-x-45 w-sm fixed bottom-5 ">
+            <div className="flex justify-center space-x-45 w-sm fixed bottom-5">
                 <Button className="h-15 mb-5" size={"lg"} asChild>
                     <Link href={"/home/score"}>SCORE</Link>
                 </Button>
@@ -39,7 +45,6 @@ const UserPage = () => {
                     <Link href={"/home/start"}>PLAY</Link>
                 </Button>
             </div>
-
         </>
     );
 };
