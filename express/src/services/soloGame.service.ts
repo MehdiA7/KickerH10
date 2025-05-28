8dac7586a287c51e169fa4c1ab99a34f382ae0a1
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-05-28 10:35:39 +0200

    add a new logic to increment user statistic

--- express/src/services/soloGame.service.ts
+++ express/src/services/soloGame.service.ts
@@ -14,9 +14,8 @@ export class SoloGameService {
         this.soloGameRepository = AppDataSource.getRepository(SoloGame);
         this.usersRepository = AppDataSource.getRepository(Users);
     }
-    
-    async createANewSoloGame(gameData: GameData): Promise<SoloGame> {
 
+    async createANewSoloGame(gameData: GameData): Promise<SoloGame> {
         // Verify user si correct
         const player1 = await this.usersRepository.findOne({
             where: { id: gameData.player1 },
@@ -41,6 +40,9 @@ export class SoloGameService {
             looser = player1;
         }
 
+        player1.goal = player1.goal + gameData.score1;
+        player2.goal = player2.goal + gameData.score2;
+
         if (winner === player1) {
             player1.xp = player1.xp + 10;
             if (player1.xp < 100) {
@@ -49,8 +51,16 @@ export class SoloGameService {
             player1.wongame = player1.wongame + 1;
             player2.lostgame = player2.lostgame + 1;
         } else {
-
+            player2.xp = player2.xp + 10;
+            if (player2.xp < 100) {
+                player2.level = player2.level + 1;
+            }
+            player2.wongame = player2.wongame + 1;
+            player1.lostgame = player1.lostgame + 1;
         }
+
+        await this.usersRepository.save(player1);
+        await this.usersRepository.save(player2);
 
         const newGame = this.soloGameRepository.create({
             player1: player1,
@@ -68,27 +78,28 @@ export class SoloGameService {
         const limit: number = 10;
         const offset: number = (page - 1) * limit;
 
-    const [allSoloGame, total] = await this.soloGameRepository
-    .findAndCount({
-        take: 10,
-        skip: offset,
-        relations: ['player1', 'player2'],
-        select: {
-            id: true,
-            score1: true,
-            score2: true,
-            player1: { id: true, username: true},
-            player2: { id: true, username: true},
-            createdat: true
-        }
-    });
+        const [allSoloGame, total] = await this.soloGameRepository.findAndCount(
+            {
+                take: 10,
+                skip: offset,
+                relations: ["player1", "player2"],
+                select: {
+                    id: true,
+                    score1: true,
+                    score2: true,
+                    player1: { id: true, username: true },
+                    player2: { id: true, username: true },
+                    createdat: true,
+                },
+            }
+        );
 
-    const formatResponse = {
-        content: allSoloGame,
-        currentPage: page,
-        totalPage: total-1
-    }
-        
-            return formatResponse;
+        const formatResponse = {
+            content: allSoloGame,
+            currentPage: page,
+            totalPage: total - 1,
+        };
+
+        return formatResponse;
     }
 }
