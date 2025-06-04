2763cd150fb04b6d1fc0b9ca745c9329b10facdd
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-06-04 11:37:19 +0200

    Add new image to all profile image

--- my-app/components/molecules/ProfileOverview.tsx
+++ my-app/components/molecules/ProfileOverview.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React, { FC } from "react";
 import footIcon from "@/public/img/footIcon.png";
+import Quentin from "@/public/img/quentin2crop.jpg";
 import { User } from "@/lib/types/type";
 import { Progress } from "../ui/progress";
 
@@ -10,12 +11,11 @@ type ProfileOverviewProps = {
 
 const ProfileOverview: FC<ProfileOverviewProps> = async ({ FetchUserProfile }) => {
     const user = await FetchUserProfile;
-    console.log(user)
     return (
         <div className="flex flex-row items-center mb-10">
             <div className="relative w-30 h-30 overflow-hidden rounded-full mr-15">
                 <Image
-                    src={footIcon}
+                    src={Quentin}
                     alt="Profile Picture"
                     fill
                     className="object-cover"
